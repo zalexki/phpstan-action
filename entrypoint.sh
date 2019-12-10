5d814b454f65ec4f6d69b80da5a23e0faf0d4f84
zalexki <haumalex@gmail.com>
2019-12-10 20:21:34 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ ls -la
 ls -la tests/phpstan
 echo $PWD
 # docker run --rm --volumes-from temp-ps -v /github/workspace:/web/module -e _PS_ROOT_DIR_=/var/www/html --workdir=/web/module phpstan/phpstan:0.11.19 analyse --configuration=/web/module/tests/phpstan/phpstan.neon;
-docker run --rm --volumes-from temp-ps -v /github/workspace:/web/module -e _PS_ROOT_DIR_=/var/www/html --workdir=/web/module php ls -la;
+docker run --rm --volumes-from temp-ps -v /github/workspace:/web/module -e _PS_ROOT_DIR_=/var/www/html --workdir=/web/module php -c sh "ls -la";
