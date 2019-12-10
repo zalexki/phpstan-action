5221bab2d65e40a8a8ffe539811c64ddf9685044
zalexki <haumalex@gmail.com>
2019-12-10 19:25:43 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@
 # composer install
 # php ~/.composer/vendor/bin/phpstan $*
 
-docker run -tid --rm -v ps-volume:/var/www/html --name temp-ps prestashop/prestashop;
+docker run -tid --rm -v ps-volume:/var/www/html --name temp-ps prestashop/prestashop:1.7.0.3;
+# docker run --rm --volumes-from temp-ps -v `pwd`:/web/module -e _PS_ROOT_DIR_=/var/www/html --workdir=/web/module ;
+ls -la
+ls -la tests/phpstan
 docker run --rm --volumes-from temp-ps -v `pwd`:/web/module -e _PS_ROOT_DIR_=/var/www/html --workdir=/web/module phpstan/phpstan:0.11.19 analyse --configuration=/web/module/tests/phpstan/phpstan.neon;
