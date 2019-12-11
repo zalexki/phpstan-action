31a1845fb0335bc7aeb337bf2071e6949aa04ef6
zalexki <haumalex@gmail.com>
2019-12-11 10:53:57 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 # php ~/.composer/vendor/bin/phpstan $*
 
 docker run -tid --rm -v ps-volume:/var/www/html --name temp-ps prestashop/prestashop:1.7.0.3;
-echo $PWD;
+ls $PWD;
 docker run --rm --volumes-from temp-ps -v /github/workspace:/web/module -e _PS_ROOT_DIR_=/var/www/html --workdir=/web/module --entrypoint "ls" phpstan/phpstan:0.11.19 -la /
 docker run --rm --volumes-from temp-ps -v /github/workspace:/web/module -e _PS_ROOT_DIR_=/var/www/html --workdir=/web/module --entrypoint "ls" phpstan/phpstan:0.11.19 -la /web/module
 docker run --rm --volumes-from temp-ps -v /github/workspace:/web/module -e _PS_ROOT_DIR_=/var/www/html --workdir=/web/module --entrypoint "ls" phpstan/phpstan:0.11.19 -la /var/www
